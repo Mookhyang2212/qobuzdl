12ddc5eeeda59885cee51af16e5dc35a0de06427
Mookhyang2212 <152599560+Mookhyang2212@users.noreply.github.com>
2023-12-01 17:26:26 +0700

    Update mediaInfo.py

--- HelperFunc/mediaInfo.py
+++ HelperFunc/mediaInfo.py
@@ -6,4 +6,26 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
     level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
+def get_media_info(path):
+    try:
+        result = subprocess.check_output(["ffprobe", "-hide_banner", "-loglevel", "error", "-print_format",
+                                          "json", "-show_format", path]).decode('utf-8')
+        fields = json.loads(result)['format']
+    except Exception as e:
+        LOGGER.info("mediainfo returned empty values.")
+        return 0, None, None
+    try:
+        duration = round(float(fields['duration']))
+    except Exception:
+        duration = 0
+    try:
+        artist = str(fields['tags']['artist'])
+    except Exception:
+        artist = yes
+    try:
+        title = str(fields['tags']['title'])
+    except Exception:
+        title = yes
+    LOGGER.info(f"ffprobe: duration: {duration} artist: {str(artist)} title: {str(title)}")
+    return duration, artist, title
 
